9dab2684360a61bcfc4a86c931f6f878c0c92299
jadoon.engr <jadoon.engr@gmail.com>
2020-12-15 07:17:43 +0400

    1

--- TestModule1.py
+++ TestModule1.py
@@ -7,5 +7,7 @@ class TestAdd(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(md1(1,2),3)
         self.assertEqual(md1(1,5),6)
+        self.assertEqual(md1(30,4),34)
+        self.assertEqual(md1(65,36),101)
      
 unittest.main()
